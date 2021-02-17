41b8ffb790af47cdf7f18a8ee01e03fd09e795e1
Ricardo Guerrero <richi_gro@hotmail.com>
2021-02-17 12:10:40 -0600

    implemented useEffect hook to and getallobjects method to get data from api and save to state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+
+//JSON file data
 import data from '../dist/InterviewProject_CareerData.json';
 
 //imported components
@@ -7,17 +10,35 @@ import Card from './components/Card';
 //stylesheets
 import './App.css';
 
-
-
 const App = () => {
-  // const careerObjects = data.data;
-  const [careerObjects, setCareerObjects] = useState(data.data);
+  //un-comment if working with local JSON file
+  // const careerObjectsFromJSONFile = data.data;
+
+  const [careerObjects, setCareerObjects] = useState({});
+
+  useEffect(() => {
+    getAllObjects();
+  }, []);
+
+  const apiURL = 'https://run.mocky.io/v3/43cf8e75-b054-4ef6-9f9f-6258dcfbe5f9';
+
+  const getAllObjects = () => {
+    Axios.get(apiURL)
+    .then((res) => {
+      return res.data.data;
+    })
+    .then((data) => {
+      setCareerObjects(data);
+    })
+    .catch((err) => {
+      console.error("Error with promise", err);
+    });
+  };
 
   console.log("career object: ", careerObjects);
   
   return (
     <div className="app-container">
-
       <ul>
         {
           Object.keys(careerObjects).map((career, index, array) => {
