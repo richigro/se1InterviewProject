6e4cdbd177d0546e9a7407e919dd9df7b28546bd
Ricardo Guerrero <richi_gro@hotmail.com>
2021-02-16 20:13:56 -0600

    Card component is now able to display its two subCard blocks that contain Median salary and topEntry level education for each career

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -14,12 +14,51 @@ const Card = ({ career }) => {
   }).sort((a,b) => b.percent - a.percent).slice(0,3);
   console.log("top: ", topEntryLevelEducation);
   return (
+
     <div className="card">
 
-      <h2>{career.title}</h2>
-      <p>{career.description}</p>
-      <img className="img-container" src={career.thumbnailUrl}/>
-      
+      <div className="card-content">
+        <h2>{career.title}</h2>
+        <p>{career.description}</p>
+
+        <div className="subCard-block-container">
+
+          {/* First sub Block with Median Salary info*/}
+          <div className="subCard-block">
+            <div className="block-header">
+              <div className="icon-container">
+                <ion-icon name="cash-outline"></ion-icon>
+              </div>
+              <p className="header-block-text">Median Salary</p>
+            </div>
+            <div className="block-content">
+              <div>
+                {/*Intl NumberFormat constrcutor */}
+                <p>${ new Intl.NumberFormat().format(Math.round(career.medianSalary))}</p>
+              </div>
+            </div>
+          </div>
+
+          {/*Second and last sub block with Top Entry Level Education info */}
+          <div className="subCard-block">
+            <div className="block-header">
+              <div className="icon-container">
+                <ion-icon name="school"></ion-icon>
+              </div>
+              <p className="header-block-text">Top Entry-level Education</p>
+            </div>
+            <div className="block-content">
+              {topEntryLevelEducation.map((education, index) => {
+                return <p key={index}>{education.level}</p>;
+              })}
+            </div>
+          </div>
+
+        </div>
+
+        <img className="img-container" src={career.thumbnailUrl}/>
+      </div>
+
     </div>
   );
 };
