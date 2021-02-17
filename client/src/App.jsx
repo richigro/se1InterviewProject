274cc7f2e2a1951d5a7e989ff525c76f442a0511
Ricardo Guerrero <richi_gro@hotmail.com>
2021-02-16 18:57:47 -0600

    retrieved data from JSON file saved it to App's state and mapped through each career

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from '../dist/InterviewProject_CareerData.json';
+
+
 
 const App = () => {
+  // const careerObjects = data.data;
+  const [careerObjects, setCareerObjects] = useState(data.data);
+  console.log("career object: ", careerObjects);
   return (
     <div>
-      This is React code
+      <ul>
+        {
+          Object.keys(careerObjects).map((career, index, array) => {
+            return <h1 key={index}>Hello</h1>;
+          })
+        }
+      </ul>
     </div>
   );
 };
