97abcdba0f347637a53afcf6e312117d337dd946
ricardo guerrero <richi_gro@hotmail.com>
2021-02-18 20:54:33 -0600

    Removed more unnecessary comments

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,4 @@ import ReactDOM from 'react-dom';
 //Components
 import App from './App';
 
-
 ReactDOM.render(<App/>, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -6,20 +6,13 @@ const DIST_DIR = path.join(__dirname, '/client/dist');
 module.exports = {
   
   entry: [`${SRC_DIR}/index.js`],
-  //this will not minify code, so that debugging is easier
   mode: 'development',
   output: {
     filename: 'bundle.js',
     path: DIST_DIR,
   },
-  
-
-  //this is what compiles my react code from the src directory
-  //and also es6 code, presets are contained within the .babelrc file
-
   module: {
     rules: [
-      //babel-loader with env and react presets
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -28,7 +21,6 @@ module.exports = {
           {
             loader: 'babel-loader',
             options: {
-              //this will avoid re-compilation if nothing changes
               cacheDirectory: true,
             },
           },
